54bb17005615d1bea6a1aa67cec4babc1eddf6d4
David Santiago <davidsghz@gmail.com>
2020-05-02 18:06:46 -0300

    migrate resposabilities

--- steps/HomeSteps.py
+++ steps/HomeSteps.py
@@ -1,40 +1,10 @@
 from pages.HomePage import HomePage
 
+#TO-DO: migrate responsibility to another Step
 class HomeSteps(object):
 
     def __init__(self):
         self.__page = HomePage()
-
-    def clickOnAboutUs(self):
-        self.__page.getAboutUs().click()
-        return self
-
-    def verifyAboutUsButton(self):
-        assert True == self.__page.getAboutUs().isDisplayed()
-        return self
-
-    def verifyPageTitle(self):
-        expectedTitle = 'About Python™ | Python.or'
-        assert expectedTitle == self.__page.getTitle() , 'Not equals. We expected: "'+expectedTitle+'" and actual is: "'+self.__page.getTitle+'"'
-        return self
-
-    def mouseOnAboutUs(self):
-        self.__page.getAboutUs().mouseOver()
-        return self
-
-    def clickOnQuotesOption(self):
-        self.__page.getQuotes().click()
-        return self
-
-    def verifyTextLearnMore(self):
-        expectedText = 'Learn More'
-        actualText = self.__page.linkLearnMore().getText()
-        assert expectedText == actualText , 'Not equals. We expected: "'+expectedText+'" and actual is: "'+actualText+'"'
-        return self
-
-    def moveUntilStatusLink(self):
-        self.__page.linkStatus().scrollUntilThis()
-        return self
 
     def goTo(self, url):
         self.__page.goToUrl(url)
