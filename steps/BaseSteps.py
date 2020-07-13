df61e763972617ff151fc31ac59b5a6e048f4b69
David Santiago <davidsghz@gmail.com>
2020-07-13 00:41:13 -0300

    refactor/move to steps folder

--- steps/BaseSteps.py
+++ steps/BaseSteps.py
@@ -20,12 +20,13 @@ class BaseStep (ABC):
         return Driver.getInstance().title
 
     @allure.step
-    def goToUrl(self, url):
+    def goToUrl(self,url=""):
         """
             A method to go at specify URL.
             :param url: str
         """
         Driver.getInstance().get(url)
+        return self
 
     @allure.step
     def closeBrowser(self):
@@ -45,6 +46,7 @@ class BaseStep (ABC):
         Assertion().assertEquals(expectedValue=titleExpected,actualValue=self.getTitle())
         return self
 
+
     @allure.step
     def takeScreenshot(self,name=str(datetime.datetime.now())):
         """
@@ -54,3 +56,35 @@ class BaseStep (ABC):
             :params name: str
         """
         allure.attach(Driver.getInstance().get_screenshot_as_png(),name,attachment_type=allure.attachment_type.PNG)
+        return self
+
+    @allure.step
+    def acceptAlert(self):
+        """
+            Accept the current dialog displayed on the browser
+        """
+        Driver.getInstance().switch_to.alert.accept()
+        return self
+
+    @allure.step
+    def declineAlert(self):
+        """
+            Cancel the current dialog displayed on the browser
+        """
+        Driver.getInstance().switch_to.alert.dismiss()
+        return self
+    
+    @allure.step
+    def __getAlertMessage(self):
+        """
+            Get message from the current dialog displayed on the browser
+        """
+        return Driver.getInstance().switch_to.alert.text
+
+    @allure.step
+    def __setTextOnAlertMessage(self, message):
+        """
+            Set text in the current dialog displayed on the browser
+        """
+        Driver.getInstance().switch_to.alert.send_keys(message)
+        
